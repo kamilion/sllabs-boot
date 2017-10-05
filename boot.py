46997bf45b3070617ab263d547166327680095d7
Kamilion <kamilion@gmail.com>
2017-10-05 01:10:36 -0700

    more work done

--- boot.py
+++ boot.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
-def hello(name=None):
-    return render_template('index.html', name=name)
+def hello():
+    return render_template('index.html', name=request.args.get('name', ''))
 
 @app.route("/template.ipxe")
 def template():
@@ -17,52 +17,75 @@ def template():
     resp.mimetype = 'text/plain'
     return resp
 
-@app.route("/localboot.ipxe")
-def localboot(name=None):
-    resp = make_response(render_template('localboot.ipxe.txt', name=name))
+@app.route("/arrival.ipxe")
+def arrival():
+    # This will set the boot-host variable in iPXE.
+    build_template = render_template('arrival.ipxe.txt',
+                        url_root=request.url_root)
+    resp = make_response(build_template)
+    resp.mimetype = 'text/plain'
+    return resp
+
+@app.route("/inventory.ipxe")
+def inventory():
+    # This will get various variables in iPXE.
+    mac_address = request.args.get('mac', '')
+    ip_address = request.args.get('ip', '')
+    uuid_address = request.args.get('uuid', '')
+
+    # do something with the collected data here.
+
+    build_template = render_template('inventory.ipxe.txt',
+        mac=mac_address, ip=ip_address, uuid=uuid_address)
+    resp = make_response(build_template)
     resp.mimetype = 'text/plain'
     return resp
 
 @app.route("/boot.ipxe")
-def bootipxe(name=None):
-    resp = make_response(render_template('boot.ipxe.txt', name=name))
+def bootipxe():
+    build_template = render_template('boot.ipxe.txt', next=request.args.get('next', ''))
+    resp = make_response(build_template)
     resp.mimetype = 'text/plain'
     return resp
 
 @app.route("/netboot.ipxe")
-def netbootipxe(name=None):
-    resp = make_response(render_template('netboot.ipxe.txt', name=name))
+def netbootipxe():
+    build_template = render_template('netboot.ipxe.txt', name=request.args.get('name', ''))
+    resp = make_response(build_template)
     resp.mimetype = 'text/plain'
     return resp
 
 @app.route("/undionly.ipxe")
-def undionlyipxe(name=None):
-    resp = make_response(render_template('undionly.ipxe.txt', name=name))
+def undionlyipxe():
+    build_template = render_template('undionly.ipxe.txt', name=request.args.get('name', ''))
+    resp = make_response(build_template)
     resp.mimetype = 'text/plain'
     return resp
 
 @app.route("/remote.ipxe")
-def remoteipxe(name=None):
-    resp = make_response(render_template('remote.ipxe.txt', name=name))
+def remoteipxe():
+    build_template = render_template('remote.ipxe.txt', name=request.args.get('name', ''))
+    resp = make_response(build_template)
     resp.mimetype = 'text/plain'
     return resp
 
 @app.route("/menu.ipxe")
-def menuipxe(name=None):
-    resp = make_response(render_template('menu.ipxe.txt', name=name))
+def menuipxe():
+    build_template = render_template('menu.ipxe.txt', name=request.args.get('name', ''))
+    resp = make_response(build_template)
     resp.mimetype = 'text/plain'
     return resp
 
 @app.route("/boot.itest")
-def bootipxetest(name=None):
-    return render_template('boot.ipxe.txt', name=name)
+def bootipxetest():
+    return render_template('boot.ipxe.txt', name=request.args.get('name', ''))
 
 
 @app.route("/google3528ba40472779da.html")
-def google(name=None):
-    return render_template('google3528ba40472779da.html', name=name)
+def google():
+    return render_template('google3528ba40472779da.html')
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
